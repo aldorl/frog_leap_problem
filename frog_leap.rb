be6627408870163f29f5478653e1c6a3cc233dc4
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-11-25 16:43:34 -0500

    Add comments and spacing to increase readability

--- frog_leap.rb
+++ frog_leap.rb
@@ -2,23 +2,30 @@ MIN_RIVER_WIDTH = 1
 MAX_RIVER_WIDTH = 100000
 
 def read_input_arguments
+  # First command-line argument, expected to be  an integer value between MIN_RIVER_WIDTH and MAX_RIVER_WIDTH
   river_width = ARGV.shift.to_i
-  raise ArgumentError, 'Argument is not an integer value between 1 and 100,000' unless river_width.is_a?(Integer) && river_width >= MIN_RIVER_WIDTH && river_width <= MAX_RIVER_WIDTH
+  raise ArgumentError, "Argument is not an integer value between #{MIN_RIVER_WIDTH} and #{MAX_RIVER_WIDTH}" unless river_width.is_a?(Integer) && river_width >= MIN_RIVER_WIDTH && river_width <= MAX_RIVER_WIDTH
   puts "The width of the river is #{river_width}"
+  
+  # Second command-line argument, expected to be the name of a file which has the positions of falling leaves
   falling_leaves = ARGF.readlines.map(&:chomp)
-  puts "Leaves fall in the following order:"
+  
   falling_leaves.each_with_index do |leaf_position, time|
     falling_leaves[time] = leaf_position.to_i
     raise ArgumentError, "Argument is not an integer value between 1 and #{river_width}" unless falling_leaves[time].is_a?(Integer) && falling_leaves[time] >= 1 && falling_leaves[time] <= river_width
     puts "At minute #{time}, a leaf falls in position #{falling_leaves[time]}"
   end
+  
   return river_width, falling_leaves
 end
 
 def calculate_earliest_leap_time(river_width, falling_leaves)
   puts "\n\nThis many leaves fall in total: #{falling_leaves.length}\n\n"
+  
+  # The unique_leaves_positions array size should be one more than the river width, given that it's a zero-indexed array
   unique_leaves_positions = Array.new(river_width+1, false)
   unique_leaves_positions_counter = 0
+  
   falling_leaves.each_with_index do |leaf_position, time|
     if unique_leaves_positions[leaf_position] == false
       unique_leaves_positions[leaf_position] = true
@@ -28,6 +35,7 @@ def calculate_earliest_leap_time(river_width, falling_leaves)
     end
     return time if unique_leaves_positions_counter == river_width
   end
+  
   return -1
 end
 
