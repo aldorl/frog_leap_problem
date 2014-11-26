996a3934c7fd0b4b4f4528edea833b97cebd88d3
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2014-11-26 12:34:04 -0500

    Implement data validation tests for FrogLeap class

--- frog_leap.rb
+++ frog_leap.rb
@@ -8,7 +8,7 @@ class FrogLeap
     # Data types validation
     raise ArgumentError, "Argument is not an integer value between #{MIN_RIVER_WIDTH} and #{MAX_RIVER_WIDTH}" unless river_width.is_a?(Integer) && river_width >= MIN_RIVER_WIDTH && river_width <= MAX_RIVER_WIDTH
     falling_leaves.each_with_index do |leaf_position, time|
-      raise ArgumentError, "Argument is not an integer value between 1 and #{river_width}" unless falling_leaves[time].is_a?(Integer) && falling_leaves[time] >= 1 && falling_leaves[time] <= river_width
+      raise ArgumentError, "Argument is not an integer value between #{MIN_RIVER_WIDTH} and #{river_width}" unless falling_leaves[time].is_a?(Integer) && falling_leaves[time] >= MIN_RIVER_WIDTH && falling_leaves[time] <= river_width
     end
     
     #Instance variables
--- spec/frog_leap_spec.rb
+++ spec/frog_leap_spec.rb
@@ -3,14 +3,46 @@ require 'spec_helper'
 describe FrogLeap do
   before :all do
     @river_width = 5
-    @short_falling_leaves = [1]
+    @min_falling_leaves = [1]
+    @short_falling_leaves = [1, 2, 3]
     @regular_falling_leaves = [1, 3, 1, 4, 2, 3, 5, 4]
+    @max_falling_leaves = Array.new(FrogLeap::MAX_RIVER_WIDTH, 1..FrogLeap::MAX_RIVER_WIDTH)
     @kermit = FrogLeap.new(@river_width, @regular_falling_leaves)
   end
   
-  it "is valid with an integer value for river_width between MIN_RIVER_WIDTH and MAX_RIVER_WIDTH" do
-    expect{FrogLeap.new(FrogLeap::MIN_RIVER_WIDTH, @short_falling_leaves)}.not_to raise_error
+  it "is valid with an integer value for river_width between MIN_RIVER_WIDTH (value #{FrogLeap::MIN_RIVER_WIDTH}) and MAX_RIVER_WIDTH (value #{FrogLeap::MAX_RIVER_WIDTH})" do
+    expect{FrogLeap.new(FrogLeap::MIN_RIVER_WIDTH, @min_falling_leaves)}.not_to raise_error
+    expect{FrogLeap.new(@river_width, @min_falling_leaves)}.not_to raise_error
+    expect{FrogLeap.new(FrogLeap::MAX_RIVER_WIDTH, @min_falling_leaves)}.not_to raise_error
+  end
+  
+  it "is valid with individual integer value for falling_leaves between MIN_RIVER_WIDTH (value #{FrogLeap::MIN_RIVER_WIDTH}) and the given river_width" do
+    expect{FrogLeap.new(@river_width, @min_falling_leaves)}.not_to raise_error
+    expect{FrogLeap.new(@river_width, @short_falling_leaves)}.not_to raise_error
     expect{FrogLeap.new(@river_width, @regular_falling_leaves)}.not_to raise_error
-    expect{FrogLeap.new(FrogLeap::MAX_RIVER_WIDTH, @regular_falling_leaves)}.not_to raise_error
+  end
+  
+  it "is invalid without a river_width value" do
+    expect{FrogLeap.new(nil, @min_falling_leaves)}.to raise_error
+  end
+  
+  it "is invalid without an array of falling_leaves value" do
+    expect{FrogLeap.new(@river_width, nil)}.to raise_error
+  end
+  
+  it "is invalid without an integer river_width value between MIN_RIVER_WIDTH (value #{FrogLeap::MIN_RIVER_WIDTH}) and MAX_RIVER_WIDTH (value #{FrogLeap::MAX_RIVER_WIDTH})" do
+    expect{FrogLeap.new("test", @min_falling_leaves)}.to raise_error
+    expect{FrogLeap.new(-1, @min_falling_leaves)}.to raise_error
+    expect{FrogLeap.new([5], @min_falling_leaves)}.to raise_error
+    expect{FrogLeap.new(5.5, @min_falling_leaves)}.to raise_error
+  end
+  
+  it "is invalid without an array of falling_leaves with individual integer values between MIN_RIVER_WIDTH (value #{FrogLeap::MIN_RIVER_WIDTH}) and the given river_width" do
+    expect{FrogLeap.new(@river_width, 1)}.to raise_error
+    expect{FrogLeap.new(@river_width, ["test"])}.to raise_error
+    expect{FrogLeap.new(@river_width, [1, 3, 1, 4, 2, 3, 0, 4])}.to raise_error
+    expect{FrogLeap.new(@river_width, [1, 3, 1, 4, 2, 3, 6, 4])}.to raise_error
+    expect{FrogLeap.new(@river_width, [1, 3, 1, 4, 2, 3, "blah", 4])}.to raise_error
+    expect{FrogLeap.new(@river_width, @max_falling_leaves)}.to raise_error
   end
 end
